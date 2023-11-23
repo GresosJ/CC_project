a31ca4893640b3941fb641d7b293ceece4528ff2
Gerson Junior <ger_son_junior@live.com.pt>
2023-11-23 10:29:38 +0000

    Fix typos

--- client/fstp.go
+++ client/fstp.go
@@ -1,87 +1,94 @@
-package fstp
+package main
 
-import {
-	"fmt"
-	"net"
-	"strings"
+import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-}
+	"fmt"
+	"net"
+)
 
 type DataBlock struct {
 	BlockID string
-	FileID string 				// Name? ou criar uma struct file com as infos?
-	Data []byte
+	FileID  string // Name? ou criar uma struct file com as infos?
+	Data    []byte
 	Hash    string
-
 }
 
 // Client -> Server(FS Node)
-func requestDataBlock(serverAddress string, blockID string, fileID string){
-	
-	conn := openUDPConn(serverAddress)
+func requestDataBlock(serverAddress string, blockID string, fileID string) {
+
+	conn, err := openUDPConn(serverAddress)
+	if err != nil {
+		fmt.Println("Erro ao abrir a conexão UDP:", err)
+		return
+	}
 	defer conn.Close()
 
-	//Formatar mensagem
-	requestMessage := fmt.Printf("REQUEST %s %s", blockID, fileID)
+	// Format the message
+	requestMessage := fmt.Sprintf("REQUEST %s %s", blockID, fileID)
 
-	//Converte a mensagem para bytes
-	data := []byte(message)
+	// Convert the message to bytes
+	data := []byte(requestMessage)
 
-	sendUDPData(data, conn, "Erro ao enviar a mensagem UDP")
+	sendUDPData(data, *conn, "Erro ao enviar a mensagem UDP")
 
 	//fmt.Println("MESSAGE SENT!")
 
-} 
+}
 
-
-//Envia um pacote 
+// Envia um pacote
 func sendDataBlock(clientAddress string, blockID string, fileID string, data []byte) {
-	
+
 	// Criar hash value atraves da data
 	hash := calculateHash(data)
 
 	datablock := DataBlock{
 		BlockID: blockID,
-		FileID: fileID,
-		Data: data,
-		Hash: hash,
+		FileID:  fileID,
+		Data:    data,
+		Hash:    hash,
 	}
 
 	//Converte a strut para bytes
 	dbBytes, err := json.Marshal(datablock)
-	if err != nil{
+	if err != nil {
 		fmt.Println("Erro ao converter Datablock para bytes", err)
 		return
 	}
 
 	// Client Addr Abrimos uma conexao por block, mas podias abrir uma conexao e passar como argumento nesta funcao
-	conn := openUDPConn(clientAddress)
+	conn, err := openUDPConn(clientAddress)
+	if err != nil {
+		fmt.Println("Erro ao abrir a conexão UDP:", err)
+		return
+	}
 	defer conn.Close()
 
 	// TODO!
 	// Datagrama vai ser igual ao UDP default
 
-	sendUDPData(datablock, conn, "Erro no envio do datablock")
+	sendUDPData(dbBytes, *conn, "Erro no envio do datablock")
 
 }
 
-//Client -> Server
-func confirmData(serverAddress string, blockID string, fileID string){
+// Client -> Server
+func confirmData(serverAddress string, blockID string, fileID string) {
 
-	conn := openUDPConn(serverAddress)
+	conn, err := openUDPConn(serverAddress)
+	if err != nil {
+		fmt.Println("Erro ao abrir a conexão UDP:", err)
+		return
+	}
 	defer conn.Close()
 
-	confirmMessage := fmt.Printf("BLOCK_CONFIRMED %s %s", blockID, fileID)
+	confirmMessage := fmt.Sprintf("BLOCK_CONFIRMED %s %s", blockID, fileID)
 
 	data := []byte(confirmMessage)
 
-	sendUDPData(data, conn, "Erro no envio da confirmacao do Datablock")
+	sendUDPData(data, *conn, "Erro no envio da confirmacao do Datablock")
 
 }
-
-
 
 func checkReceivedDataBlock(data []byte) {
 
@@ -106,27 +113,26 @@ func checkReceivedDataBlock(data []byte) {
 	}
 }
 
-
 //////////////////// Utils Functions ////////////////////
 
-func openUDPConn(addr string) *UDPConn{
-	serverAddr, err := net.ResolveUDPAddr("udp", addr) 
+func openUDPConn(addr string) (*net.UDPConn, error) {
+	serverAddr, err := net.ResolveUDPAddr("udp", addr)
 	if err != nil {
 		fmt.Println("Erro ao obter o IP:", err)
-		return
+		return nil, err
 	}
 
-	//Abre ligacao UDP
-	conn, err := net.DialUDP("udp",nil,addr)
+	// Abre ligacao UDP
+	conn, err := net.DialUDP("udp", nil, serverAddr)
 	if err != nil {
 		fmt.Println("Erro ao abrir ligacao UDP", err)
-		return
+		return nil, err
 	}
 
-	return conn
+	return conn, nil
 }
 
-func sendUDPData(data []byte, conn *UDPConn, errorMessage string){
+func sendUDPData(data []byte, conn net.UDPConn, errorMessage string) {
 	_, err := conn.Write(data)
 	if err != nil {
 		fmt.Println(errorMessage, err)
@@ -141,8 +147,7 @@ func calculateHash(data []byte) string {
 	return hash
 }
 
-
-/*	
+/*
 	ToAsk:
 		Como vamos fzr em relacao ao sequencializacao dos ficheiros, para os transformar em blocos
 
